574a569cc87d7454eb737e8fa4fba8a0e8165df6
2049419 <2049419@student.uma.pt>
2022-03-09 20:09:30 +0000

    Update ficha01.js

--- ficha01.js
+++ ficha01.js
@@ -1,6 +1,5 @@
 //5.
-function disciplina(teorica, pratica)
-{
+function disciplina(teorica, pratica) {
     //notas de 0 a 20
     nota_teorica = teorica * 0.6;
     nota_pratica = pratica * 0.4;
@@ -15,8 +14,7 @@ disciplina(12, 11);
 
 
 //6.
-function mes(numero)
-{
+function mes(numero) {
     //1-12
     switch (numero) {
         case 1:
@@ -56,38 +54,36 @@ function mes(numero)
             console.log("dezembro");
             break;
         default:
-            console.log("Mês não Existente");    
+            console.log("Mês não Existente");
     }
 }
 mes(12);
 
 
 //7.
-function operadores(num1, num2, op)
-{
-    if (op == "+"){
+function operadores(num1, num2, op) {
+    if (op == "+") {
         res = num1 + num2;
         console.log(res);
-    } else if (op == "-"){
+    } else if (op == "-") {
         res = num1 - num2;
         console.log(res);
-    } else if (op == "*"){
+    } else if (op == "*") {
         res = num1 * num2;
         console.log(res);
-    } else if (op == "/"){
+    } else if (op == "/") {
         res = num1 / num2;
         console.log(res);
-    } else if (op == "^"){
-        res = num1**num2;
+    } else if (op == "^") {
+        res = num1 ** num2;
         console.log(res);
     }
 }
-operadores(2,2, "^");
+operadores(2, 2, "^");
 
 
 //8.
-function multiplos()
-{
+function multiplos() {
     i = 1;
     while (i < 4) {
         multi = i * 5;
@@ -99,9 +95,9 @@ multiplos();
 
 
 //9.
-function soma100(){
+function soma100() {
     res = 0;
-    for (i = 1; i <= 100 ;i++){
+    for (i = 1; i <= 100; i++) {
         res += i;
     }
     console.log(res);
@@ -110,28 +106,66 @@ soma100();
 
 
 //10.
-function factorial(n){
-    res = n*n-1;
-    console.log(res);
+function factorial(n) {
+    if (n < 0) {
+        console.log("Não é possivel fazer factorial de um numero negativo.")
+    }
+    else if (n == 0) {
+        console.log("0")
+    }
+    else {
+        fact = 1;
+        for (i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        console.log(fact)
+    }
 }
 factorial(3);
 
 
 //11.
-function verify_array(array){
-    for (i = 0; i < array.length; i++){
-        if (array[i] < 0){
+function verify_array(array) {
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
             console.log("Erro:Numero negativo encontrado");
         } else {
-            max = 0;
-            for (j = 0; j <= max; j++){
-                if (array[j] > max) {
-                    max = array[i]
-                    console.log("Biggest number: ", max);
-                }  
-            }
+            biggest(array);
+            smallest(array);
+            media(array);
+            break;
         }
     }
 }
+function biggest(array) {
+    var max = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (max < array[i]) {
+            max = array[i];
+        }
+    }
+    console.log("Maior:", max);
+    //max = Math.max.apply(null, array);
+    //console.log("Maior:", max);
+}
+function smallest(array) {
+    var min = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (min > array[i]) {
+            min = array[i];
+        }
+    }
+    console.log("Maior:", min);
+    //min = Math.min.apply(null, array);
+    //console.log("Menor:", min);
+}
+function media(array) {
+    var total = 0;
+    for (i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    avg = total / array.length;
+    console.log("Média:", avg);
+}
 
-verify_array([1,2,3,-3,4])
+verify_array([1, 2, 3, 3, 4])
