8a2533aaae9d5f79588fb80dc38540874aa1c5a4
2049419 <2049419@student.uma.pt>
2022-03-14 20:56:30 +0000

    Update ficha02.js

--- ficha02.js
+++ ficha02.js
@@ -36,8 +36,9 @@ reverse("Hoje e Domingo")
 //3.
 function vowels(phrase2) {
     var numberVowels = 0;
-    for (var i = 0; i < phrase2.length; i++) {
-        if (phrase2[i] == "a" || phrase2[i] == "e" || phrase2[i] == "i" || phrase2[i] == "o" || phrase2[i] == "u") {
+    var lowerCasePhrase2 = phrase2.toLowerCase();
+    for (var i = 0; i < lowerCasePhrase2.length; i++) {
+        if (lowerCasePhrase2[i] == "a" || lowerCasePhrase2[i] == "e" || lowerCasePhrase2[i] == "i" || lowerCasePhrase2[i] == "o" || lowerCasePhrase2[i] == "u") {
             numberVowels += 1;
         }
     }
@@ -47,14 +48,35 @@ vowels("Hoje e Domingo")
 
 //4.
 function letter(phrase3, letter) {
+    var lowerCasePrase3 = phrase3.toLowerCase();
+    var lowerCaseLetter = letter.toLowerCase();
     var numberLetters = 0;
-    for (var i = 0; i < phrase3.length; i++) {
-        if (phrase3[i] == letter) {
+    for (var i = 0; i < lowerCasePrase3.length; i++) {
+        if (lowerCasePrase3[i] == lowerCaseLetter) {
             numberLetters += 1;
         }
     }
-    console.log("The letter", letter, "has appeared", numberLetters, "times.")
+    console.log(phrase3);
+    console.log("The letter", letter, "has appeared", numberLetters, "times.");
 }
-letter("Hoje e Domingo", "o")
+letter("Hoje e Domingo", "O")
 
 //5.
+function work(entry, exit) { //08:12:30
+    var splitEntry = entry.split(":");
+    var splitExit = exit.split(":");
+    var hours = splitExit[0] - splitEntry[0];
+    var minutes = splitExit[1] - splitEntry[1];
+    var seconds = splitExit[2] - splitEntry[2];
+    if (splitEntry[0] >= 8 && splitExit[0] <= 18) {
+        if (minutes < 0 || minutes > 60) {
+            hours += 1;
+        } else if (seconds < 0 || seconds > 60) {
+            minutes += 1;
+        }
+    console.log("Work time was:", hours,":",minutes,":",seconds)
+    } else {
+        console.log("Wrong time(s) were inputed.");
+    }
+}
+work("8:12:30", "17:20:30")
