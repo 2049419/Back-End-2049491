f9b4649d8522672d2f6c124d9f37ebd8a85da6b4
2049419 <2049419@student.uma.pt>
2022-04-06 19:09:12 +0100

    ficha4

---
+++ ficha4/person.js
@@ -0,0 +1,31 @@
+//3.
+
+//a)
+function Person(firstName, lastName){
+    //b)
+    this.firstName = firstName,
+    this.lastName = lastName;
+}
+
+//c)
+Person.prototype.greet = function() {
+        console.log("Hello " + this.firstName + " " + this.lastName);
+}
+
+//d)
+var john = new Person("John", "Doe");
+var jane = new Person("Jane", "Doe");
+john.greet();
+jane.greet();
+
+//e))
+Person.prototype.age = null;
+var john2 = new Person("John", "Doe");
+john2.age = 30;
+console.log(john2);
+
+//g))
+Person.prototype.greet2 = function() {
+    console.log("Hello " + this.firstName + " " + this.lastName + ", you are " + this.age + " years old.");
+}
+john2.greet2();
