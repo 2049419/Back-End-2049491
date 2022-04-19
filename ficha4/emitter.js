f42e23974c46c0cd71e530b8d84ca052f2ec658d
2049419 <2049419@student.uma.pt>
2022-04-19 19:45:35 +0100

    Update emitter.js

--- ficha4/emitter.js
+++ ficha4/emitter.js
@@ -7,7 +7,9 @@ class Emitter {
 }
 
 Emitter.prototype.on = function(type, listener) {
+    if (this.events[type] == undefined) {
     this.events[type] = [];
+    }
     this.events[type].push(listener);
 }
 
