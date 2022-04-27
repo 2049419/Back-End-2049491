70a7db275499c36cfd6e98bb853f277cad41d70a
2049419 <2049419@student.uma.pt>
2022-04-27 20:57:41 +0100

    Update server.js

--- ficha5/server.js
+++ ficha5/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 const port = 3000;
 
 function readFile(path){
@@ -8,16 +13,47 @@ function readFile(path){
     return fileContent;
 }
 
-console.log(readFile('./persons.json'));
 
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+  }
+)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/users', function(request, response) {
+    var fileContent = readFile('./persons.json');
+    response.send(fileContent);
+  }
+)
 
-app.get('/users', (req, res) => {
-    res.send('Hello Users!')
-  })
+app.post('/users', function(request, response) {
+  
+    var person = request.body;
+    
+    var personString = readFile('./persons.json');
+
+    var personsObject = JSON.parse(personString);
+
+    var size = Object.keys(personsObject).length;
+    size++;
+    var str = 'person';
+    var personId = str + size;
+
+    person.id = size;
+
+    personsObject[personId] = person;
+    response.send(personsObject);
+  }
+)
+
+app.put('/users', function(request, response) {
+  response.send("THIS IS A PUT");
+  }
+)
+
+app.delete('/users', function(request, response) {
+  response.send("THIS IS A DELETE");
+  }
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
