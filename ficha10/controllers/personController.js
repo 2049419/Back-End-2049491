5a1c7d9767351cbf576493b5e55d600005ab483a
2049419 <2049419@student.uma.pt>
2022-06-13 21:00:57 +0100

    ficha10

---
+++ ficha10/controllers/personController.js
@@ -0,0 +1,11 @@
+const Person = require("../sequelize").Person;
+
+exports.getAllPerson() = function (req, res, next) {
+    Person.findAll().then(result => {
+        res.send(result);
+    })
+}
+
+exports.test = function(req, res, next ) {
+    res.send("TEST");
+}
---
+++ ficha10/sequelize.js
@@ -0,0 +1,11 @@
+const {
+    Sequelize,
+    Model,
+    DataType,
+    DataTypes,
+    EmptyResultError
+  } = require("sequelize")
+  const express = require("express");
+  const {
+    response
+  } = require("express");
